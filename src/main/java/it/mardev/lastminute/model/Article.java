333cb14e8dcbfa2feb897138f8423db9e7c551d4
Marco Piccolo <mpiccolo@begear.it>
2020-12-12 16:45:07 +0100

    print method tests

--- src/main/java/it/mardev/lastminute/model/Article.java
+++ src/main/java/it/mardev/lastminute/model/Article.java
@@ -24,13 +24,6 @@ public class Article implements Taxable, Printable {
 		float salesTax = salesPrice / (100 + getTaxRate()) * getTaxRate();
 		salesTax = Math.round(salesTax * 20) / 20;
 
-//		int actualRate = Taxes.Sales.getRate() + (imported ? Taxes.Imported.getRate() : 0);
-//		Double salesPrice = price * actualRate / 100;
-//		Round to nearest .05
-//		(Math.round(price * 20) / 20) || 0.25 * Math.round(price * 20)
-//		DecimalFormat df = new DecimalFormat("####0.00");
-//		System.out.println("Value: " + df.format(value));
-
 		return salesTax;
 	}
 
@@ -45,7 +38,7 @@ public class Article implements Taxable, Printable {
 
 	@Override
 	public String print() {
-		return String.format("%d %s %s: %f", quantity, !imported ? "" : "imported", name, salesPrice);
+		return String.format("%d %s%s: %.2f", quantity, !imported ? "" : "imported ", name, salesPrice);
 	}
 
 }
--- src/test/java/it/mardev/lastminute/model/ArticleTest.java
+++ src/test/java/it/mardev/lastminute/model/ArticleTest.java
@@ -1,7 +1,6 @@
 package it.mardev.lastminute.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,25 +9,37 @@ class ArticleTest {
 	@Test
 	void given_generic_article_not_imported_get_standard_duty_rate_of_10() {
 		Article article = new Article(1, "generic", 10, false);
-		assertEquals(10, article.getTaxRate(),"Standard duty rate should be 10%");
+		assertEquals(10, article.getTaxRate(), "Standard duty rate should be 10%");
 	}
 
 	@Test
 	void given_generic_article_imported_get_standard_duty_rate_of_15() {
 		Article article = new Article(1, "generic", 10, true);
-		assertEquals(15, article.getTaxRate(),"Standard duty rate for Imported should be 15%");
+		assertEquals(15, article.getTaxRate(), "Standard duty rate for Imported should be 15%");
 	}
 
 	@Test
 	void given_generic_article_not_imported_with_sales_price_of_11_get_sales_tax_of_1() {
 		Article article = new Article(1, "generic", 11, false);
-		assertEquals(1, article.getSalesTax(),"Standard Sales Tax should be 1€");
+		assertEquals(1, article.getSalesTax(), "Standard Sales Tax should be 1€");
 	}
 
 	@Test
 	void given_generic_article_imported_with_sales_price_of_23_get_sales_tax_of_3() {
 		Article article = new Article(1, "generic", 23, true);
-		assertEquals(3, article.getSalesTax(),"Standard Sales Tax for Imported should be 3€");
+		assertEquals(3, article.getSalesTax(), "Standard Sales Tax for Imported should be 3€");
+	}
+
+	@Test
+	void given_generic_article_not_imported_should_print() {
+		Article article = new Article(1, "generic", 10, false);
+		assertEquals("1 generic: 10,00", article.print());
+	}
+
+	@Test
+	void given_generic_article_imported_should_print() {
+		Article article = new Article(1, "generic", 10, true);
+		assertEquals("1 imported generic: 10,00", article.print());
 	}
 
 }
