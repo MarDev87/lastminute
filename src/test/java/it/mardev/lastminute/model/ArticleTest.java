0c21c8dfd8c4d4fff955af6a796612a9014f57aa
Marco Piccolo <mpiccolo@begear.it>
2020-12-12 17:23:57 +0100

    SpringBootTests + pom ogranization

--- src/test/java/it/mardev/lastminute/model/ArticleTest.java
+++ src/test/java/it/mardev/lastminute/model/ArticleTest.java
@@ -3,7 +3,9 @@ package it.mardev.lastminute.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ArticleTest {
 
 	@Test
