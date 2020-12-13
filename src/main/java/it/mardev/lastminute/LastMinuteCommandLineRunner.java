2c64e54fb47ce91cbe81c00130e618017e5f0151
Marco Piccolo <mpiccolo@begear.it>
2020-12-13 23:53:38 +0100

    finalized CLI applicatino

--- src/main/java/it/mardev/lastminute/LastMinuteCommandLineRunner.java
+++ src/main/java/it/mardev/lastminute/LastMinuteCommandLineRunner.java
@@ -2,10 +2,12 @@ package it.mardev.lastminute;
 
 import java.util.Scanner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import it.mardev.lastminute.factory.ArticleFactory;
 import it.mardev.lastminute.model.Article;
 import it.mardev.lastminute.model.Receipt;
 import it.mardev.lastminute.model.ShoppingBasket;
@@ -14,8 +16,11 @@ import it.mardev.lastminute.model.ShoppingBasket;
 @Component
 public class LastMinuteCommandLineRunner implements CommandLineRunner {
 
+	@Autowired
+	private ArticleFactory factory;
+
 	private ShoppingBasket shoppingBasket = new ShoppingBasket();
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
@@ -26,47 +31,98 @@ public class LastMinuteCommandLineRunner implements CommandLineRunner {
 			System.out.println("2: Add Article");
 			System.out.println("3: Remove Article");
 			System.out.println("4: Print Receipt");
-			System.out.println("5: Exit");
+			System.out.println("5: Empty Basket");
+			System.out.println("9: Exit");
 
-			String choice = scanner.next();
+			String choice = scanner.nextLine();
 			try {
 				int intChoice = Integer.parseInt(choice);
 				switch (intChoice) {
 				case 1:
-					shoppingBasket.print();
+					System.out.println(shoppingBasket.printWithLineNumber());
 					break;
 				case 2:
-					shoppingBasket.addArticle(readArticle(scanner));
+					Article article = readArticle(scanner);
+					if (article != null) {
+						shoppingBasket.addArticle(article);
+					}
 					break;
 				case 3:
-					shoppingBasket.removeArticle(removeArticle(scanner));
+					removeArticle(scanner);
 					break;
 				case 4:
 					Receipt receipt = new Receipt(shoppingBasket);
 					receipt.printToConsole();
 					break;
 				case 5:
+					shoppingBasket = new ShoppingBasket();
+					System.out.println("Basket emptied");
+					break;
+				case 9:
 					System.out.println("Closing program");
+					scanner.close();
 					return;
 				default:
-					System.out.println("Choice incorrect!!!");
+					System.err.println("Choice incorrect!!!");
 					break;
 				}
 			} catch (Exception e) {
-				System.out.println("Choice incorrect!!!");
+				System.err.println("Choice incorrect!!!");
 			}
 		}
 
 	}
 
-	private Article removeArticle(Scanner scanner) {
-		// TODO Auto-generated method stub
-		return null;
+	private void removeArticle(Scanner scanner) {
+		System.out.print("\nInsert article number:");
+		String index = scanner.nextLine();
+		if (index.equalsIgnoreCase("EXIT")) {
+			System.out.println("Operation aborted");
+		}
+		try {
+			int iIndex = Integer.parseInt(index) - 1;
+			if (iIndex > shoppingBasket.getArticles().size()) {
+				System.err.println("Invalid line inserted");
+				removeArticle(scanner);
+			} else {
+				System.out.println("Article removed");
+				System.out.println(shoppingBasket.removeArticle(iIndex).print());
+			}
+		} catch (Exception e) {
+			System.err.println("Invalid line inserted");
+			removeArticle(scanner);
+		}
 	}
 
 	private Article readArticle(Scanner scanner) {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.print("\nInsert article name or 'EXIT' to return to menu:");
+		String name = scanner.nextLine();
+		if (name.equalsIgnoreCase("EXIT")) {
+			System.out.println("Operation aborted");
+			return null;
+		}
+		float salesPrice = getPrice(scanner);
+		if (salesPrice == -1) {
+			System.out.println("Operation aborted");
+			return null;
+		}
+		return factory.getArticle(1, name, salesPrice);
+	}
+
+	private float getPrice(Scanner scanner) {
+		System.out.print("\nInsert sales price [###,##] or 'EXIT' to return to menu");
+		String salesPrice = scanner.nextLine();
+		if (salesPrice.equalsIgnoreCase("EXIT")) {
+			return -1;
+		}
+		try {
+			float fSalesPrice = Float.parseFloat(salesPrice);
+			return fSalesPrice;
+		} catch (Exception e) {
+			System.err.println("Invalid format inserted");
+			getPrice(scanner);
+		}
+		return -1;
 	}
 
 }
