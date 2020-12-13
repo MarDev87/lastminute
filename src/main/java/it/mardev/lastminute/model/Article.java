289cbd08b626a13219724fd826ac65e87aa2e1fb
Marco Piccolo <mpiccolo@begear.it>
2020-12-13 23:35:51 +0100

    fixed roundup alghoritm

--- src/main/java/it/mardev/lastminute/model/Article.java
+++ src/main/java/it/mardev/lastminute/model/Article.java
@@ -20,7 +20,7 @@ public class Article implements Taxable, Printable {
 	public float getSalesTax() {
 		// TODO CALCOLARE LE TASSE A PARTIRE DAL RATE E DAL PREZZO DI VENDITA
 		float salesTax = salesPrice / (100) * getTaxRate();
-		salesTax = Math.round(salesTax * 20) / 20f;
+		salesTax = ((float)Math.ceil(salesTax * 20)) / 20f;
 
 		return salesTax;
 	}
@@ -36,7 +36,7 @@ public class Article implements Taxable, Printable {
 
 	@Override
 	public String print() {
-		return String.format(Locale.UK, "%d %s%s: %.2f", quantity, !imported ? "" : "imported ", name,
+		return String.format(Locale.UK, "%d %s: %.2f", quantity, name,
 				salesPrice + getSalesTax());
 	}
 
