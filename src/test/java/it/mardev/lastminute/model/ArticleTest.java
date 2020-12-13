9882d28d242cdc2c47738a3291fdb6b5ce3c2cfe
Marco Piccolo <mpiccolo@begear.it>
2020-12-13 03:38:53 +0100

    unnecessary spring context on tests

--- src/test/java/it/mardev/lastminute/model/ArticleTest.java
+++ src/test/java/it/mardev/lastminute/model/ArticleTest.java
@@ -3,9 +3,7 @@ package it.mardev.lastminute.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ArticleTest {
 
 	@Test
--- src/test/java/it/mardev/lastminute/model/DutyFreeArticleTest.java
+++ src/test/java/it/mardev/lastminute/model/DutyFreeArticleTest.java
@@ -1,11 +1,9 @@
 package it.mardev.lastminute.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DutyFreeArticleTest {
 
 	@Test
